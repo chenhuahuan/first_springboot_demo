02f46fb3d60ea0e4ad4c7efa490c306071200000
hunter chen <chenhuahuan@icloud.com>
2024-05-05 16:08:28 +0800

    更新首个springboot demo

---
+++ src/main/java/com/mydemo/springboot/FirstSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mydemo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringbootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mydemo/springboot/FirstSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydemo.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
